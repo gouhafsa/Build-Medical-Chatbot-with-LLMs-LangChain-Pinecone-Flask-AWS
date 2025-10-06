2148ae81b0936b9f776f47ecfd98ca85d78baa3b
Hafsa Gourmat <gouhafsa7@gmail.com>
2025-10-06 22:51:50 +0100

    folder structure added

---
+++ template.sh
@@ -0,0 +1,17 @@
+# Creatind directories for a new project
+mkdir -p src
+mkdir -p research
+
+# Creatind files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+
+
+echo "Directory amd files created successfully."
