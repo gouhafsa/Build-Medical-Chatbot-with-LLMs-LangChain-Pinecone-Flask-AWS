f81f3a3bfcec60dd928f691bfbcb56140a59b634
Hafsa Gourmat <gouhafsa7@gmail.com>
2025-10-07 16:28:33 +0100

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Hafsa Gourmat",
+    author_email="gouhafsa7@gmail.com",
+    packages=find_packages(),
+    install_requires=[]          
+
+)
